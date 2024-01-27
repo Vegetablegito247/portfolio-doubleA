9bae7a6a197902b5ac7c902401cb07af4d040068
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2024-01-27 02:27:20 -0600

    first commit

---
+++ src/store/store.js
@@ -0,0 +1,8 @@
+import { configureStore, combineReducers } from "@reduxjs/toolkit";
+
+const rootReducer = combineReducers({});
+
+export const store = configureStore({
+    reducer: rootReducer,
+});
+
