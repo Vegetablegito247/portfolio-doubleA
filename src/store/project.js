5bb882f37b0dd25644a8382380809fe3430d3926
Vegetablegito247 <Yoshimitsu247Kakarot@gmail.com>
2024-02-10 17:24:59 -0600

    added a new project

--- src/store/project.js
+++ src/store/project.js
@@ -3,11 +3,23 @@ import frontEndTutor from '../assets/portfolioImg/frontend-tutor.png';
 import blog from '../assets/portfolioImg/hulk1.png';
 import pastryAndIcecream from '../assets/portfolioImg/pastryandice.png';
 import queekai from '../assets/portfolioImg/queekai.png';
+import apronClone from '../assets/portfolioImg/apron-clone.png';
 
 const initialState = {
     projects: [
         {
             id: '01',
+            img: apronClone,
+            title: 'Apron',
+            info: "Welcome to Apron, your ultimate solution for seamless invoice management. With Apron, small business owners can effortlessly streamline invoice processing, from consolidation to payment and reconciliation. Say goodbye to administrative hassles and hello to smooth, efficient operations. Start your journey with Apron today!",
+            stack: [
+                'React', 'React-scroll', 'Redux-Persist', 'React-redux', 'Redux-toolkit', 'Framer-motion'
+            ],
+            gitHub: 'https://github.com/Vegetablegito247/apron',
+            website: 'https://apron-alpha.vercel.app'
+        },
+        {
+            id: '02',
             img: blog,
             title: 'Hulkify (Blog Template)',
             info: "Unleash the power of fitness knowledge with my Blog Template. Featuring expert articles by renowned authors like John Cena and Dr. Stone, dive into effective muscle building, optimal nutrition, cardio benefits, workout routines, rest importance, and achieving fitness goals. Explore now for a healthier, more informed fitness journey!",
@@ -18,7 +30,7 @@ const initialState = {
             website: 'https://christmasspecial.vercel.app'
         },
         {
-            id: '02',
+            id: '03',
             img: queekai,
             title: 'Queekk AI',
             info: 'Discover QUEEKK, where real-time communication and lightning-fast responses come together to redefine the way you connect and collaborate.',
@@ -29,7 +41,7 @@ const initialState = {
             website: 'https://queekk.vercel.app'
         },
         {
-            id: '03',
+            id: '04',
             img: pastryAndIcecream,
             title: 'Double A Pastry and Ice-cream',
             info: 'Explore our pastry journey via our meticulously crafted website. Enjoy seamless shopping with React-Redux, Redux Persist, and React-Paystack. Discover more in our sweet blog!',
@@ -40,7 +52,7 @@ const initialState = {
             website: 'https://react-redux-pastry-and-icecream-site.vercel.app'
         },
         {
-            id: '04',
+            id: '05',
             img: frontEndTutor,
             title: 'FrontEnd-Tutor',
             info: 'FrontEnd-Tutor offers video tutorials, challenges, quizzes for front-end dev, empowering skill enhancement. Built with React, Django, and varied libraries.',
